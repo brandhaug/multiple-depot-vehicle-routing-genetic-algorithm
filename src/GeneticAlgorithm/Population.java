d0724b759c6a9ef5965055d6d8bf091b987d7953
brandhaug <mbrandhaug@live.no>
2019-01-29 21:13:48 +0100

    - Reformat

--- src/GeneticAlgorithm/Population.java
+++ src/GeneticAlgorithm/Population.java
@@ -13,8 +13,7 @@ import java.util.stream.Collectors;
 /**
  * Population contains n Solutions. n = populationSize
  */
-public class Population
-{
+public class Population {
     private List<Depot> depots;
     private List<Solution> solutions = new ArrayList<>();
     private Solution alphaSolution; // Best Solution (with best fitness)
@@ -36,8 +35,7 @@ public class Population
      * @param mutationRate
      * @param selectionRate
      */
-    public Population(List<Depot> depots, int populationSize, double crossOverRate, double mutationRate, double selectionRate)
-    {
+    public Population(List<Depot> depots, int populationSize, double crossOverRate, double mutationRate, double selectionRate) {
         this.depots = depots;
         this.populationSize = populationSize;
         this.crossOverRate = crossOverRate;
@@ -56,16 +54,13 @@ public class Population
      * 2. Mutation
      * 3. Calculate distance and fitness
      */
-    public void tick()
-    {
+    public void tick() {
         //selection();
 
         List<Solution> children = new ArrayList<>();
-        for (Solution solution : solutions)
-        {
+        for (Solution solution : solutions) {
             double random = Utils.randomDouble();
-            if (random < crossOverRate)
-            {
+            if (random < crossOverRate) {
                 //solutions.add(new Solution(depots, solution.crossOver())); // parents need to be removed, otherwise two new cars is always added to a solution
                 Solution partner = findCrossOverPartner(solution);
 
@@ -81,8 +76,7 @@ public class Population
                 children.add(new Solution(depots, solution.crossOver(routeFromS2)));
                 children.add(new Solution(depots, partner.crossOver(routeFromS1)));
             }
-            if (random < mutationRate)
-            {
+            if (random < mutationRate) {
                 children.add(new Solution(depots, solution.mutation()));
             }
         }
@@ -95,12 +89,11 @@ public class Population
         generation++;
     }
 
-    Solution findCrossOverPartner(Solution self)
-    {
+    Solution findCrossOverPartner(Solution self) {
         Solution partner = self;
         while (self == partner) {
-        partner = solutions.get(Utils.randomIndex(solutions.size() - 1));
-    }
+            partner = solutions.get(Utils.randomIndex(solutions.size() - 1));
+        }
         return partner;
     }
 
@@ -111,10 +104,8 @@ public class Population
      * @param crossOverRate
      * @param mutationRate
      */
-    private void generateInitialPopulation(int populationSize, double crossOverRate, double mutationRate)
-    {
-        for (int i = 0; i < populationSize; i++)
-        {
+    private void generateInitialPopulation(int populationSize, double crossOverRate, double mutationRate) {
+        for (int i = 0; i < populationSize; i++) {
             Solution solution = new Solution(this.depots);
             solutions.add(solution);
         }
@@ -125,10 +116,8 @@ public class Population
      * Selects n best Solutions. n = populationRoute to keep populationSize fixed
      * TODO: Select parents for next generation before the the population is filtered
      */
-    public void selection()
-    {
-        if (Controller.verbose)
-        {
+    public void selection() {
+        if (Controller.verbose) {
             System.out.println("========= Performing selection on vehicles =========");
             System.out.println("Vehicles size before selection: " + solutions.size());
         }
@@ -137,36 +126,31 @@ public class Population
         //this.alphaSolution = solutions.get(0); // Best Solution
 
         // TODO: Select parents
-        for (int i = 0; i < solutions.size() * selectionRate; i++)
-        {
+        for (int i = 0; i < solutions.size() * selectionRate; i++) {
             Solution parent = solutions.get(i);
         }
 
         // Filters Solutions to populationSize. Because the list is sorted, the Solutions with worst fitness will be filtered
         solutions = solutions.stream().limit(populationSize).collect(Collectors.toList());
 
-        if (Controller.verbose)
-        {
+        if (Controller.verbose) {
             System.out.println("Sorted solutions distances: " + solutions.toString());
             System.out.println("Solutions size before selection: " + solutions.size());
             System.out.println("========= END Performing selection on solutions =========");
         }
     }
 
-    public int getAlphaFitness()
-    {
+    public int getAlphaFitness() {
         return alphaSolution.getFitness();
     }
 
-    public Solution getAlphaSolution()
-    {
+    public Solution getAlphaSolution() {
         solutions.sort(Comparator.comparingDouble(Solution::getFitness)); // Sorts based on fitness
         alphaSolution = solutions.get(0); // Best Solution
         return alphaSolution;
     }
 
-    public int getGeneration()
-    {
+    public int getGeneration() {
         return generation;
     }
 }
